400f8ac297fc16e4a24412f48799e926c92f4ab1
Elihan303 <elihanth@hotmail.com>
2021-03-10 15:58:39 -0400

    First commit

---
+++ scripts.js
@@ -0,0 +1,37 @@
+
+//variables
+let btn = document.getElementById("btnGenerar");
+let proceso = document.getElementById("Proceso");
+let resultado = document.getElementById("Resultado");
+
+//funcion generar tabla
+const generarTabla = () => {
+    //obtencion del valor del imput
+    let numero = document.getElementById("GNumero").value;
+    //limpienza de la tabla en caso de entrar un nuevo numero
+    proceso.innerHTML = "";
+    resultado.innerHTML = "";
+   
+    //generador de resultados
+    for (let i = 1; i < 11; i++) {
+        //validacion de si es cero
+        if(numero==0){
+            break;
+        }
+        proceso.innerHTML += `<td><h5>${numero}x${i} = </h5></td>`;
+        resultado.innerHTML += `<td><h5> ${numero * i}</h5></td>`;
+    }
+
+}
+
+//evento boton
+btn.addEventListener('click', generarTabla);
+
+
+
+
+
+
+
+
+
